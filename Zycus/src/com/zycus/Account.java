74f5be20f9a59312306b5bb1ee83d77b35543551
amit-viradiya <37014565+amit-viradiya@users.noreply.github.com>
2018-03-03 23:45:50 +0530

    Foramtted code

--- Zycus/src/com/zycus/Account.java
+++ Zycus/src/com/zycus/Account.java
@@ -2,38 +2,36 @@ package com.zycus;
 
 import java.util.ArrayList;
 
-public class Account 
-{ 
+public class Account {
 	double interest;
-public Account() 
-{ 
-	this.interest = 0;
-}
-public double interest(int amount)
-{
-	if(amount > 0 && amount < 1001){
-		interest = 0;
-	} else if (amount > 1000 && amount < 5001) {
-		interest = (amount * 0.025);
-	}else if (amount > 5000 && amount < 10001) {
-		interest = (amount * 0.05);
-	}else if (amount > 10000) {
-		interest = (amount * 0.08);
-	}
-	return interest;
-	
-}
-public int interestType(ArrayList<Double> interests) 
-{
-	int zeroInterestAccounts = 0;
-	for(Double interest : interests) {
-		if(interest == 0 ){
-			zeroInterestAccounts += 1;
-		}
-	}
-	return zeroInterestAccounts; 
-// code to determine accounts with no interest goes here 
-// this is test comment 
-} 
-}
 
+	public Account() {
+		this.interest = 0;
+	}
+
+	public double interest(int amount) {
+		if (amount > 0 && amount < 1001) {
+			interest = 0;
+		} else if (amount > 1000 && amount < 5001) {
+			interest = (amount * 0.025);
+		} else if (amount > 5000 && amount < 10001) {
+			interest = (amount * 0.05);
+		} else if (amount > 10000) {
+			interest = (amount * 0.08);
+		}
+		return interest;
+
+	}
+
+	public int interestType(ArrayList<Double> interests) {
+		int zeroInterestAccounts = 0;
+		for (Double interest : interests) {
+			if (interest == 0) {
+				zeroInterestAccounts += 1;
+			}
+		}
+		return zeroInterestAccounts;
+		// code to determine accounts with no interest goes here
+		// this is test comment
+	}
+}
--- Zycus/src/com/zycus/AccountTest.java
+++ Zycus/src/com/zycus/AccountTest.java
@@ -1,51 +1,52 @@
 package com.zycus;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class AccountTest {
 
-	public static int findNoOfAccounts(String sid){
+	public static int findNoOfAccounts(String sid) {
 		int greatest = Integer.MIN_VALUE;
 		for (int i = 0; i < sid.length(); i++) {
 			int value = greatest;
-		    try {
-		    	value = Integer.parseInt(String.valueOf(sid.charAt(i)));	
-		    }catch(NumberFormatException e){
-		    	continue;
-		    }
-		    greatest = Math.max(greatest, value);
+			try {
+				value = Integer.parseInt(String.valueOf(sid.charAt(i)));
+			} catch (NumberFormatException e) {
+				continue;
+			}
+			greatest = Math.max(greatest, value);
 		}
 		return greatest;
 	}
-	
+
 	public static void main(String[] args) {
 		Account a = new Account();
 		System.out.println("Welcome to your Banking App");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your Student ID: ");
-		String  sid = sc.next();
+		String sid = sc.next();
 		System.out.println("Student ID you entered is: " + sid);
 		int noOfAccounts = findNoOfAccounts(sid);
 		System.out.println(noOfAccounts);
 		int[] accounts = new int[noOfAccounts];
-		for(int i = 0; i < noOfAccounts; i++) {
+		for (int i = 0; i < noOfAccounts; i++) {
 			System.out.println("Enter the amount for Acc No " + (i + 1) + ": ");
-			accounts[i] = sc.nextInt();	
-			System.out.println("The interest for account number " + (i+1) + " is " + a.interest(accounts[i]));
+			accounts[i] = sc.nextInt();
+			System.out.println("The interest for account number " + (i + 1) + " is " + a.interest(accounts[i]));
 		}
 		double sumOfInterst = 0;
-		ArrayList<Double>intrests= new ArrayList<>();
-		for( int num : accounts) {	
-		 	double ba =a.interest(num);
-		 	sumOfInterst += ba;
-		 	intrests.add(ba);      
-	    }
+		ArrayList<Double> intrests = new ArrayList<>();
+		for (int num : accounts) {
+			double ba = a.interest(num);
+			sumOfInterst += ba;
+			intrests.add(ba);
+		}
 		System.out.println("----------Interest Details-----------");
-	    System.out.println("Total Interest : "+sumOfInterst);
-	    System.out.println("Lowest Interest: " + Collections.min(intrests));
-	    System.out.println("Highest Interest: " + Collections.max(intrests));
-	    System.out.println("Average Interest:"+ (sumOfInterst/accounts.length));  
-	    System.out.println("Number of accounts with no interest: " + a.interestType(intrests));
-	}	
+		System.out.println("Total Interest : " + sumOfInterst);
+		System.out.println("Lowest Interest: " + Collections.min(intrests));
+		System.out.println("Highest Interest: " + Collections.max(intrests));
+		System.out.println("Average Interest:" + (sumOfInterst / accounts.length));
+		System.out.println("Number of accounts with no interest: " + a.interestType(intrests));
+	}
 }
